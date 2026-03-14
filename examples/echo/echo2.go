a4eef0eccc8458d54716f8000448af47f10d5d3f
weaselfiest <weaselfiest@gmail.com>
2026-03-14 16:36:01 +0500

    add: code examples from GOPL ch.1

---
+++ examples/echo/echo2.go
@@ -0,0 +1,17 @@
+// Echo1 выводит аргументы командной строки
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ examples/fetch/fetchall.go
@@ -0,0 +1,40 @@
+// Fetchall параллельно получает URL и сообщает их время и размер
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // запуск горутины
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // получение из канала ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // отправка в канал ch
+		return
+	}
+
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
