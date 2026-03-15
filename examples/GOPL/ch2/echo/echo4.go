3196c4afc82d2fe4a72d547b6447ee6d91690288
weaselfiest <weaselfiest@gmail.com>
2026-03-16 00:44:48 +0500

    add: examples from GOPL ch2

---
+++ examples/GOPL/ch2/echo/echo4.go
@@ -0,0 +1,19 @@
+// Echo4 выводит аргументы командной строки
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "пропуск символа новой строки")
+var sep = flag.String("s", " ", "разделитель")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
